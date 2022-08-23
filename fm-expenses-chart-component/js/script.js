21097525d0bb6a272081e8cbf866faa6553c19f4
brodiewebdt <81633541+brodiewebdt@users.noreply.github.com>
2022-08-23 15:20:39 -0400

    upload current version of project

--- fm-expenses-chart-component/js/script.js
+++ fm-expenses-chart-component/js/script.js
@@ -1,28 +1,20 @@
 fetch("../data.json")
     .then((res) => res.json())
     .then((data) => {
-        console.log(data);
-
+        // Adds the JSON data to the bar chart spans
         const span = document.querySelectorAll("span");
-        console.log(span);
 
         for (let i = 0; i < span.length; i++) {
             span[i].dataset.title = data[i].day;
             span[i].dataset.amount = "$" + data[i].amount;
             span[i].style.height = data[i].amount * 3 + "px";
 
-            const day = new Date().getDay(); //return 0 to 6
+            // Get the current day to show the correct day in the bar chart
+            const day = new Date().getDay(); //returns 0 to 6
             let weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
             if (weekday[day].toLowerCase() === data[i].day) {
-                span[i].classList.add("current-day")
+                span[i].classList.add("current-day");
             }
-        }
-
-        let total = 0;
-
-        for (let x = 0; x < data.length; x++) {
-            total += data[x].amount;
-            console.log(total / 100);
         }
     });
